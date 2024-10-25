7b428be94062742ee45e3085a85d7e1d3c58b38e
Lizi138 <lizimchedlidze245@gmail.com>
2024-10-25 16:58:22 +0400

    first commit

---
+++ level 03/classwork/classwork.py
@@ -0,0 +1,9 @@
+name = "Lizi"
+
+Lastname = "Mchedlidze"
+
+age = "11"
+
+Animal = "dog"
+
+print(name, Lastname, age, Animal)
---
+++ level 03/homework/homework2.py
@@ -0,0 +1,4 @@
+print(20 + 20)
+print(20 * 7)
+print(100 - 84)
+print(100 / 10)
