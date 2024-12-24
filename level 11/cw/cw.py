02254969af139749ad0bf1f4da756e2b54c9f29e
Lizi138 <lizimchedlidze245@gmail.com>
2024-12-24 13:58:54 +0400

    second commit

---
+++ level 11/cw/cw.py
@@ -0,0 +1,58 @@
+# პირველი დავალება
+
+# >-ის 10 მაგალითი
+print(2 > 3)
+print(4 > 3)
+print(5 > 6)
+print(4>5)
+print(9>2)
+print(6>3)
+print(2>9)
+print(10>8)
+print(3>5)
+print(6>9)
+
+# <-ის 10 მაგალითი
+print(9<2)
+print(3<5)
+print(4<5)
+print(7<9)
+print(8<3)
+print(2<4)
+print(9<4)
+print(4<6)
+print(7<8)
+print(9<3)
+
+# ==-ის 10 მაგალითი
+print(10==3)
+print(9==2)
+print(2==2)
+print(5==6)
+print(3==4)
+print(5==6)
+print(9==1)
+print(1==1)
+print(5==6)
+print(3==6)
+
+# !=-ის 10 მაგალითი
+print(9!=0)
+print(3!=9)
+print(3!=4)
+print(4!=7)
+print(8!=7)
+print(11!=9)
+print(7!=6)
+print(3!=9)
+print(4!=3)
+print(6!=3)
+print(9!=3)
+
+
+#მეორე დავალება
+x = "Hello"
+a = "Hello"
+
+print(x==a)
+print(x!=a)
---
+++ level 11/hw/hw.py
@@ -0,0 +1,122 @@
+# დავალება 1
+
+# >-ის 15 მაგალითი
+print(2 > 7)
+print(4 > 3)
+print(5 > 0)
+print(4>5)
+print(9>9)
+print(6>3)
+print(2>5)
+print(10>8)
+print(3>8)
+print(6>9)
+print(4>1)
+print(9>2)
+print(6>8)
+print(2>0)
+print(10>8)
+
+# <-ის 15 მაგალითი
+print(9<1)
+print(3<6)
+print(4<5)
+print(7<0)
+print(8<3)
+print(2<2)
+print(9<7)
+print(4<2)
+print(7<8)
+print(9<3)
+print(8<3)
+print(2<1)
+print(9<4)
+print(4<2)
+print(7<9)
+
+# !=-ის 15 მაგალითი
+print(9!=0)
+print(3!=1)
+print(3!=1)
+print(4!=7)
+print(8!=7)
+print(11!=11)
+print(7!=6)
+print(3!=3)
+print(4!=3)
+print(6!=2)
+print(9!=9)
+print(9!=1)
+print(1!=9)
+print(0!=4)
+print(6!=7)
+print(8!=7)
+
+# ==-ის 15 მაგალითი
+print(10==3)
+print(9==6)
+print(2==2)
+print(5==0)
+print(3==7)
+print(5==6)
+print(9==1)
+print(1==9)
+print(5==2)
+print(3==8)
+print(5==6)
+print(9==0)
+print(1==1)
+print(5==2)
+print(3==6)
+
+# დავალება 2
+
+#1
+a = "Hello"
+b = "hello"
+print(a!=b)
+
+#2
+a = "dog"
+b = "DOg"
+print(a != b)
+
+#3
+a = "school"
+b = "School"
+print(a != b)
+
+#4
+a = "mum"
+b = "MUM"
+print(a != b)
+
+#5
+a = "bike"
+b = "BIkE"
+print(a != b)
+
+#6
+a = "world"
+x = "word"
+print(a == x)
+
+#7
+a = "book"
+b = "BOOOK"
+print(a == b)
+
+#8
+a = "note"
+b = "Note"
+print(a == b)
+
+#9
+a = "girl"
+b = "Girl"
+print(a == b)
+
+#10
+a = "Good"
+b = "good"
+print(a == b)
