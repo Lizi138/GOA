516df790b2993e454bb86f656fcff57f2510a0b9
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-08 19:16:47 +0400

    second commit

---
+++ level 12/cw/cw.py
@@ -0,0 +1,21 @@
+# დავალება 1
+name = "Lizi"
+Lastname = "Mchedlidze"
+Age = "11"
+Favorite_song = "My Way"
+Favorite_food = "Khinkali"
+print(type(name))
+print(type(Lastname))
+print(type(Age))
+print(type(Favorite_song))
+print(type(Favorite_food))
+
+# დავალება 2
+num1 = int(input("შეიყვანე პირველი რიცხვი: "))
+num2 = int(input("შეიყვანე მეორე რიცხვი: "))
+print(float(num1))
+print(float(num2))
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
