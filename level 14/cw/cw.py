fc6796a3cd4d6d278e19a121ce798fd7079fd052
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-08 11:24:43 +0400

    second commit

---
+++ level 14/cw/cw.py
@@ -0,0 +1,34 @@
+# საკლასო დავალება 1
+num1 = int(input("შეიყვანეთ რიცხვი: "))
+print(num1 < 100)
+print(num1 > 50)
+print(num1 == 25)
+
+# საკლასო დავალება 2
+# >-ის 5 მაგალითი
+print(22 > 7)
+print(4 > 3)
+print(11 > 0)
+print(4>5)
+print(9>9)
+
+# <-ის 5 მაგალითი
+print(3<1)
+print(3<6)
+print(4<8)
+print(5<0)
+print(8<1)
+
+# ==-ის 5 მაგალითი
+print(3==3)
+print(9==1)
+print(2==2)
+print(5==4)
+print(3==7)
+
+# !=-ის 5 მაგალითი
+print(11!=0)
+print(3!=4)
+print(1!=1)
+print(4!=2)
+print(13!=7)
