b26fca41de35f2d113551bc08276bf7b8fecb79f
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-09 21:57:23 +0400

    second commit

--- level 16/hw/hw.py
+++ level 16/hw/hw.py
@@ -1,1 +1,63 @@
+# დავალება 1
+print(5 < 3 and 5 > 3)
+print(1 > 2 and 1 < 2)
+print(3 > 2 and 2 < 3)
+print(4 > 1 and 4 < 1)
+print(6 > 9 and 6 < 9)
+
+print(1 < 2 or 1 > 2)
+print(9 < 7 or 9 > 7)
+print(5 < 7 or 5 > 7)
+print(0 > 4 or 0 < 4)
+print(3 < 5 or 3 > 5)
+
+# დავალება 2
+a = 5
+b = 10
+print(a < b and b != 10)
+
+x = 7
+y = 3
+z = 7
+print(x == z or y > 10)
+
+a = 15
+b = 15
+c = 20
+print(a == b and b > c)
+
+x = 5
+y = 8
+z = 5
+print((x == z or y != z) and x < y)
+
+a = 100
+b = 200
+c = 50
+print(a < b and c >= 50) 
+
+x = 3
+y = 3
+z = 5
+print( x != y and z > x)
+
+a = 10
+b = 15
+print(a < b or b == 20)
+
+a = 7
+b = 7
+print( (a != b or b < 10))
+
+x = 4
+y = 6
+z = 8
+print(x < y and y < z) 
+
+a = 20
+b = 30
+c = 15
+print((a >= b or b != c) and (a == b))
+
+
 
---
+++ level 18/cw/cw.py
@@ -0,0 +1,22 @@
+# დავალება 1
+range1 = range(10) #0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+print(range1)  
+range2 = range(16) #0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+print(range1)
+range3 = range(20) #0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19
+print(range1)
+
+range4 = range(2, 11) #2, 3, 4, 5, 6, 7, 8, 9, 10
+print(range1)
+range5 = range(4, 7) #4, 5, 6
+print(range5)
+range6 = range(3, 16) #3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15
+print(range6)
+
+range7 = range(2, 8, 2) #2, 4, 6
+print(range7)
+range8 = range(10, 41, 5) #10, 15, 20, 25, 30, 35, 40
+print(range8)
+range9 = range(3, 16, 3) #3, 6, 9, 12, 15
+print(range9)
+
