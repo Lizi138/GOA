7d2fafb8a0b7bbbb1ed4387dd46e83992035d085
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-10 20:31:03 +0400

    second commit

---
+++ level 20/cw/cw.py
@@ -0,0 +1,14 @@
+# # დავალება 1
+# for i in range(10):
+#     print("Lizi Mchedlidze")
+# # დავალება 2
+# for i in range(0, 21, 2):
+#     print(i)
+# # დავალება 3
+# for x in range(11, 40, 2):
+#     print(x)
+# # დავალება 4
+numbers_sum = 0
+for i in range(10, 31, 2):
+    numbers_sum = numbers_sum + i
+print(numbers_sum)
