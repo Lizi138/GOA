1b473214da7f04144887f83b3b709b5912e52c8a
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-12 22:08:15 +0400

    second commit

---
+++ level 24/cw/cw.py
@@ -0,0 +1,15 @@
+# საკლასო 1
+num1 = int(input("შეიყვანე რიცხვი: "))
+if num1 > 10:
+    print("Bigger than 10")
+else:
+    print("Smaller than 10")
+
+
+# საკლასო 2
+word = input("Is the person a student?: ")
+
+if word == "yes":
+    is_student = True
+else:
+    is_student = False
---
+++ level 24/hw/hw.py
@@ -0,0 +1,184 @@
+# += ოპერატორის მაგალითები
+x = 5
+x += 3  
+print(x)  
+
+y = 10
+y += 5  
+print(y)  
+
+a = 2
+a += 4  
+print(a)  
+
+b = 1
+b += 7 
+print(b)  
+
+c = 15
+c += 10  
+print(c) 
+
+d = 100
+d += 50  
+print(d)  
+
+e = 0
+e += 100  
+print(e) 
+
+f = -5
+f += 10 
+print(f)  
+
+g = 50
+g += 25  
+print(g) 
+
+h = 20
+h += 80
+print(h)  
+
+# -= ოპერატორის მაგალითები
+i = 10
+i -= 2  
+print(i) 
+
+j = 50
+j -= 10  
+print(j)  
+
+k = 100
+k -= 30  
+print(k)  
+
+l = 200
+l -= 50  
+print(l)  
+
+m = 30
+m -= 5  
+print(m) 
+
+n = 0
+n -= 5  
+print(n) 
+
+o = 60
+o -= 25  
+print(o)  
+
+p = -10
+p -= 10  
+print(p)  
+
+q = 500
+q -= 100 
+print(q)  
+
+r = 120
+r -= 30  
+print(r) 
+
+# *= ოპერატორის მაგალითები
+s = 2
+s *= 3  
+print(s)  
+
+t = 4
+t *= 5  
+print(t)  
+
+u = 6
+u *= 2 
+print(u)  
+
+v = 7
+v *= 3  
+print(v) 
+
+w = 3
+w *= 4 
+print(w)  
+
+x = 5
+x *= 6  
+print(x)  
+
+y = 8
+y *= 2  
+print(y)  
+
+z = 10
+z *= 10  
+print(z)  
+
+aa = 1
+aa *= 50  
+print(aa)  
+
+bb = 3
+bb *= 4 
+print(bb)  
+
+# /= ოპერატორის მაგალითები
+cc = 10
+cc /= 2  
+print(cc) 
+
+dd = 20
+dd /= 4  
+print(dd) 
+
+ee = 30
+ee /= 3   
+print(ee)  
+
+ff = 100
+ff /= 5  
+print(ff)  
+
+gg = 50
+gg /= 2  
+print(gg) 
+
+hh = 90
+hh /= 3 
+print(hh)  
+
+ii = 75
+ii /= 15  
+print(ii)  
+
+jj = 200
+jj /= 4 
+print(jj)  
+
+kk = 120
+kk /= 12  
+print(kk)  
+
+ll = 1000
+ll /= 10  
+print(ll)  
+
+# >= ამ ოპერატორის დახმარებით შევადარებთ ორ რიცხვს და ვამოწმებთ, არის თუ არა პირველი რიცხვთან მეტი ან ტოლი მეორე რიცხვთან.
+# <= ამ ოპერატორის დახმარებით შევადრებთ ორ რიცხვს და ვამოწმებთ, არის თუ არა პირველი რიცხვი ნაკლები ან ტოლი მეორე რიცხვთან.
+# პირობითი განცხადებები გვაძლევს საშუალებას, რომ შევამოწმოთ პრიობები და შემდეგ სხვადასხვა მოქმედებები შევასრულოთ 
+
+
+# დავალება 5
+user_num = int(input("Enter a number: "))
+
+if user_num > 0:
+    print("The number is positive.")
+else:
+    print("The number is negative")
+  
+# დავალება 6
+
+user_num1 = int(input("შემოიტანე რიცხვი: "))
+if user_num1 > 100:
+    print("Big number")
+else:
+    print("Small Number")
