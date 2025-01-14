9d3448ab91db7fa7f193cf30aea09c4361052534
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-14 18:10:16 +0400

    second commit

---
+++ level 28/cw/cw.py
@@ -0,0 +1,24 @@
+# საკლასო 1
+cars = ["BMW", "MERCEDES", "PORSCHE", "AUDI", "TESLA"]
+print(cars[0])
+print(cars[1])
+print(cars[2])
+print(cars[3])
+print(cars[4])
+
+# საკლასო 2
+integer_list = [10, 24, 57]
+print(integer_list[0])
+print(integer_list[1])
+print(integer_list[2])
+
+print(integer_list)
+integer_list[0] = 5
+print(integer_list)
+integer_list[2] = 23
+print(integer_list[0] + integer_list[2])
+
+# საკლასო 3
+num1 = int(input("Enter Any Number: "))
+num_list = [2, 6, 21, 29, 34, 47, 12, 93, 11, 4]
+print(num_list[num1])
---
+++ level 28/hw/hw.py
@@ -0,0 +1,52 @@
+# დავალება 1
+# list - არის სია რომელშიც გვინდა ბევრი ინფორმაციის შენახვა
+# index - ვიყენებთ სიის ელემენტის პოზიციის დასანომრად
+# indexing - ბეჭდავს სიაში არსებულ ნებისმიერ ელემენტს, რომელასაც ჩვენ ვეუბნებით
+# mutable - ელემენტი რომელიც ცვალებადია
+# immutable - შეუცვლელი
+# დავალება 2
+
+numbers_list = [1, 2, 3, 4, 5]
+print(numbers_list[1])
+print(numbers_list[3])
+print(numbers_list[4])
+
+# დავალება 3
+
+letter_list = [1.3, 2.1, 3.2, 8.3, 2.2, 9.4, 1.0, 22.1, 38.84, 19.2, 11.22, 13.2, 1.3, 44.3, 32, 23.3, 11.3, 23.4, 10.9, 32.4 ]
+print(letter_list[0])
+print(letter_list[1])
+print(letter_list[2])
+print(letter_list[3])
+print(letter_list[4])
+print(letter_list[5])
+print(letter_list[6])
+print(letter_list[7])
+print(letter_list[8])
+print(letter_list[9])
+print(letter_list[10])
+print(letter_list[11])
+print(letter_list[12])
+print(letter_list[13])
+print(letter_list[14])
+print(letter_list[15])
+print(letter_list[16])
+print(letter_list[17])
+print(letter_list[18])
+print(letter_list[19])
+
+# დავალება 4
+# Creating a list of five numbers
+my_list = [10, 20, 30, 40, 50]
+
+print(my_list[-5])  
+print(my_list[-3])  
+print(my_list[-1])   
+
+
+# დავალება 5
+name = input("Enter your Name: ")
+first_lett = name[0]
+last_lett = name[-1]
+print("first letter is", first_lett)
+print("last letter is", last_lett)
