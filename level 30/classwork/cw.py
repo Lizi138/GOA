0a324bb025dbc56790bee731e4bd67d79c08daae
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-20 20:59:12 +0400

    second commit

---
+++ level 30/classwork/cw.py
@@ -0,0 +1,35 @@
+ # დავალება 1 (1)
+# list1 = ["Good", "Job", 12.3, 43.2, 21, 49, True, False, [1, 2, 3], [3, 2, 1]]
+# print(list1[0:9])
+# print(list1[1:4:7])
+
+ # დავალება 1
+list2 = [10, 2.3, 4, True, "abc", 23, 64, 12.4, 14, 21, 22, 40.2, 12, 43.5, 21.23]
+print(list2[::-1])
+# დავალება 2
+print(list2[1:3])
+print(list2[3:4])
+print(list2[1:6])
+print(list2[2:6])
+print(list2[2:3])
+# დავალება 3
+print(list2[2:3:1])
+print(list2[3:4:2])
+print(list2[1:2:4])
+print(list2[2:2:1])
+print(list2[2:5:2])
+# დავალება 4
+print(list2[:3])
+print(list2[:1])
+print(list2[:5])
+print(list2[:6])
+print(list2[:4])
+
+
+# საკლასო 2
+list3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(list3[-3:])
+print(list3[-1:])
+print(list3[-5:])
+print(list3[-9:])
+print(list3[-4:])
