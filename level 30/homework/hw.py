fff4553aa1a124ad3f4f0ff576c342195e20b607
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-21 15:28:10 +0400

    second commit

---
+++ level 30/homework/hw.py
@@ -0,0 +1,40 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+elements = my_list[:5]
+print(elements)
+
+my_string = "Hello, World!"
+chars = my_string[-3:]
+print(chars)
+
+element = my_list[::2]
+print(element)
+
+third_element = my_list[::3]
+print(third_element)
+
+last_element = my_list[-1]
+print(last_element)
+
+first_to_last = my_string[1:-1]
+print(first_to_last)
+
+middle_to_end = my_list[3:8]
+print(middle_to_end)
+
+full_of_list = my_list[:]
+print(full_of_list)
+
+reversed_string = my_string[::-1]
+print(reversed_string)
+
+first_half = my_list[0:4]
+print(first_half)
+
+excluding_first_three = my_list[3:]
+print(excluding_first_three)
+
+string = my_string[1:-1]  
+print(string)
+
+last_five_elements = my_list[-5:]
+print(last_five_elements)
