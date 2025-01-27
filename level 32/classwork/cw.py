fe573de653c074d90a1ffec6adb64ed1f5648426
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-27 14:32:23 +0400

    second commit

---
+++ level 32/classwork/cw.py
@@ -0,0 +1,27 @@
+# საკლასო 1
+def greet(name):
+    print("გამარჯობა", name, "!")
+greet("ნიკა")
+greet("მარიამი")
+greet("გიო")
+greet("გიორგი")
+greet("ანა")
+
+# საკლასო 2
+def multiply(num1, num2):
+    print(num1 * num2)
+multiply(21, 4)
+multiply(2, 5)
+multiply(12, 9)
+multiply(4, 6)
+multiply(54, 3)
+
+# საკლასო 3
+def my_range(num1, num2):
+    for i in range(num1, num2  + 1):
+        print(i)
+my_range(1, 6)
+my_range(10, 16)
+my_range(20, 26)
+my_range(30, 36)
+my_range(100, 106)
