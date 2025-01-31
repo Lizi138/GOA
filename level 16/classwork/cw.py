5966841a63882d37cae9519339a6d00ecc5d74bc
Lizi138 <lizimchedlidze245@gmail.com>
2025-01-31 18:55:04 +0400

    second commit

---
+++ level 16/classwork/cw.py
@@ -0,0 +1,45 @@
+# საკლასო 1
+a = True
+b = False
+print(a and b)  
+
+a = True
+b = False
+print(a or b)  
+
+a = True
+print(not a)  
+
+a = 5
+b = 5
+print(a == b) 
+
+a = 5
+b = 3
+print(a != b)  
+
+a = 3
+b = 5
+print(a < b)  
+
+a = 3
+b = 5
+print(a > b)  
+
+a = 3
+b = 3
+print(a <= b)  
+
+a = 5
+b = 5
+print(a >= b)  
+
+a = True
+b = False
+print(a and (b or True)) 
+
+# საკლასო 2
+
+number = int(input("შეიყვანე მთელი რიცხვი: "))
+
+print(number > 10 or False)
