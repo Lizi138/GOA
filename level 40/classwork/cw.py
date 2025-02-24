d8797b0b22238e8599bbeabfbbc1e51ec1ce7d24
Lizi138 <lizimchedlidze245@gmail.com>
2025-02-24 12:54:58 +0400

    second commit

---
+++ level 40/classwork/cw.py
@@ -0,0 +1,17 @@
+def greet(name, surname, age, academy, favourite_color, city):
+    return f"Hello, my name is {name}, my surname is {surname}. I am {age} years old. I study in {academy}. My favourite color is {favourite_color}. I live in {city}."
+result = greet("Lizi", "Mchedlidze", 11, "GOA", "Black", "Tbilis")
+
+# codewars
+# multiply
+#  def multiply(a, b):
+#     return a * b
+
+# Even or Odd
+# def even_or_odd(number):
+#     if number % 2 == 0:
+#         return "Even"
+#     else:
+#         return "Odd"
+
+# 
---
+++ level 40/homework/hw.py
@@ -0,0 +1,33 @@
+# codewars
+# Convert a Numbre to a String!
+# def number_to_string(num):
+#      return str(num)
+
+# even or odd
+# def even_or_odd(number):
+# if number % 2 == 0:
+#     return "Even"
+# else:
+#     return "Odd"
+
+# reversed strings
+# def solution(string):
+# return string[::-1]
+
+# REturn Negative
+# def make_negative( number ):
+# if number > 0:
+#     return number * -1
+# else:
+#     return number
+
+# Opposite Number
+# def opposite(number):
+# return number * -1
+
+# Convert boolean values to strings "yes" or "no".
+# def bool_to_word(boolean):
+# if boolean == True:
+#     return "Yes"
+# else:
+#     return "No"
