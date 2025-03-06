771ba934282dfb328dc3c37660c5e34886bee5ff
Lizi138 <lizimchedlidze245@gmail.com>
2025-03-06 13:19:21 +0400

    second commit

---
+++ level 44/homework/hw.py
@@ -0,0 +1,124 @@
+# Codewars
+# Convert a String to a Number!
+# def string_to_number(s):
+# return int(s)
+
+# Grasshopper - Summation
+# def summation(num):
+# result=0
+# for i in range(1,num+1):
+#     result=result+i
+# return  result
+
+# Function 1 - hello world
+# def greet():
+# return "hello world!"
+
+# Counting sheep...
+# def count_sheeps(sheep):
+# return sheep.count(True)
+
+# Remove String Spaces
+# def no_space(x):
+#     result = ""
+#     for i in x:
+#         if i != " ":
+#             result += i
+#     return result
+# 2
+# def no_space(x):
+#     return x.replace(" " , "")
+
+# You Can't Code Under Pressure #1
+# def double_integer(i):
+# return i*2
+
+# Returning Strings
+# def greet(name):
+#     return f"Hello, {name} how are you doing today?
+
+# Convert a Boolean to a String
+# def boolean_to_string(b):
+#     return str(b)
+# def boolean_to_string(b):
+#     if b == True: return "True"
+#     return "False"
+
+# Basic Mathematical Operations
+# def basic_op(operator, value1, value2):
+#     if operator == "+":
+#         return value1 + value2
+#     elif operator == "-":
+#         return value1 - value2
+#     elif operator == "":
+#         return value1 value2
+#     else:
+#         return value1 / value2
+
+# Keep Hydrated!
+# def litres(time):
+#     return int(time*(0.5))
+# time*(0.5) --> 0.8
+# int(0.8) --> 0
+
+# time*(0.5) --> 2.2
+# int(2.2) --> 2
+
+# Century From Year
+# def century(year):
+# if year % 100 == 0:
+#     return int(year / 100)
+# else:
+#     return int(year / 100) + 1
+
+# Convert number to reversed array of digits
+# def digitize(n):
+#     str_number = str(n)
+#     rev_number = str_number[::-1]
+
+#     new_list = []
+#     for number in rev_number:
+#         new_list.append(int(number))
+
+#     return new_list
+
+# ერთ-ერთი ამოცანის გარჩევა
+# def no_space(x):
+#     return x.replace(" " , "")
+# ეს ამოცანა წაუშლის სფეისებს ნებისმიერ წინადადებას, იგივე ამოაცლის
+
+# code wars hw
+# Beginner - Lost Without a Map
+# def maps(a):
+#     return [x * 2 for x in a]
+
+# a = [1, 2, 3]
+# result = maps(a)
+# print(result)
+
+# opposites attract
+# def lovefunc(flower1, flower2):
+#     return (flower1 % 2 == 0 and flower2 % 2 != 0) or (flower1 % 2 != 0 and flower2 % 2 == 0)
+
+# flower1 = 4
+# flower2 = 5
+# result = lovefunc(flower1, flower2)
+# print(result)
+
+# begginer series #1 school paperwork
+# def paperwork(n, m):
+#     if n < 0 or m < 0:
+#         return 0
+#     return n * m
+
+# print(paperwork(5, 5)) 
+# print(paperwork(-5, 5))
+# print(paperwork(3, 4)) 
+# print(paperwork(0, 10))
+
+# MakeUpperCase
+# def make_upper_case(s):
+#     return s.upper()
+
+# print(make_upper_case("hello"))
+# print(make_upper_case("wolrd!"))
