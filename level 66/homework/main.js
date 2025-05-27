1b0aee85f0c57e0ff65fda80b4ba091cba86f6ee
Lizi138 <lizimchedlidze245@gmail.com>
2025-05-27 17:41:13 +0400

    second commit

---
+++ level 66/homework/main.js
@@ -0,0 +1,64 @@
+// 1
+function numSum() {
+    let num1 = 231
+    let num2 = 31
+    let res = num1 + num2
+    console.log(res)
+}
+
+// 2
+function numbersSum() {
+    let numb1 = 425
+    let numb2 = 84
+    let res2 = numb1 - numb2
+    console.log(res2)
+}
+
+// 3
+function divdNums() {
+    let nm1 = 114
+    let nm2 = 65
+    let res3 = nm1 * nm2
+    console.log(res3)
+}
+
+// 4
+function halfNUms() {
+    let number1 = 75
+    let number2 = 5
+    let res4 = nm1 / nm2
+    console.log(res4)
+}
+
+// 5
+function halfNUms() {
+    let numbers1 = 75
+    let numbers2 = 12
+    let numbers3 = 6574
+    let res5 = numbers1 + numbers2 + numbers3
+    console.log(res5)
+}
+
+// 6
+let a = 54;
+let b = 5;
+console.log(a == b); 
+
+// 7
+let c = 193;
+let d = 4;
+console.log(c > d); 
+
+// 8
+let e = 12;
+let f = 94;
+console.log(e <= f);
+
+// 9
+let g = 9932;
+let h = 1;
+console.log(g != h); 
+
+// 10
+let i = 128;
+console.log(i >= 100); 
