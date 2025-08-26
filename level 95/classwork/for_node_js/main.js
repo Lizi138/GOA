4ad38fd013c0d17c5d65088dcfebf51ef5b2af9a
Lizi Mchedlidze <lizimchedlidze245@gmail.com>
2025-08-26 17:32:33 +0400

    second commit

---
+++ level 95/classwork/for_node_js/main.js
@@ -0,0 +1,10 @@
+const str = "madam";
+
+for (let i=0; i<str.length / 2; i++){
+    if (str != str.length-1){
+        return false;
+    } else {
+        return true
+    }
+}
+
---
+++ level 95/homework/main.js
@@ -0,0 +1,22 @@
+// 1 +
+console.log("Lizi Mchedlidze");
+console.log("I am 12 yo");
+console.log("You can see your terminal");
+
+// 2 + (DONE)
+
+// 3 + (done)
+
+// 4 + (done)
+
+// 5 + (done)
+
+// 6 + (done)
+
+// 7 + (done)
+
+// 8 + (done) its -v
+
+// 9 + (done)
+
+// 10 + (done)
